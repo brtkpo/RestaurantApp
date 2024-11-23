f93d4c9ef99661f004d9b30430482df2ad26f225
brtkpo <bartoszpoko@gmail.com>
2024-11-23 21:26:26 +0100

    added restaurant and product classes without migration

--- backend/core/models.py
+++ backend/core/models.py
@@ -76,3 +76,24 @@ class Address(models.Model):
     class Meta:
         verbose_name = "Address"
         verbose_name_plural = "Addresses"
+        
+class Restaurant(models.Model):
+    owner = models.OneToOneField(AppUser, on_delete=models.CASCADE, limit_choices_to={'role': 'restaurateur'})
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    description = models.TextField(null=True, blank=True)
+    #created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.restaurant.name}"
