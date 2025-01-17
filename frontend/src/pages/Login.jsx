f8f4e3780ab0ad9b51edc6b8607e8eab9e5a1028
brtkpo <bartoszpoko@gmail.com>
2025-01-17 17:53:55 +0100

    added material ui xD

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import Button from '@mui/material/Button';
 
 const Login = () => {
   return (
     <div>
       <h1>Panel logowania</h1>
       <LoginForm />
+      <Button variant="contained" color="primary">
+        Zaloguj się
+      </Button>
     </div>
   );
 };
